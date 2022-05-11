80fd52195935eb302f5fcdefe16cf4296fc6a784
TheD0g3TV <matyaszavora@outlook.com>
2022-05-11 09:11:03 +0200

    a

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,13 @@
+import UI.App;
+
+import javax.swing.*;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("ahoj");
+            App okno = new App();
+            okno.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+            okno.setVisible(true);
+            okno.setSize(300, 200);
+            okno.setLocationRelativeTo(null);
     }
 }
---
+++ src/UI/App.java
@@ -0,0 +1,11 @@
+package UI;
+
+import javax.swing.*;
+
+public class App extends JFrame{
+    JPanel mainPanel;
+    public App()
+    {
+        super("Moje první okno"); //parametr je titulek okna
+    }
+}
