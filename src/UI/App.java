3c20a2e86d0fe86b8a165a74c9b61dc215ec7161
TheD0g3TV <matyaszavora@outlook.com>
2022-05-15 21:39:53 +0200

    c

--- src/UI/App.java
+++ src/UI/App.java
@@ -8,40 +8,52 @@ import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 import java.io.IOException;
 
-
 public class App extends JFrame implements ActionListener{
-    JPanel mainPanel;
-    JButton buttonExit = new JButton();
-    JButton buttonStart = new JButton();
-    JLabel main = new JLabel();
 
-    public App() throws IOException { //počáteční "podmínky" okna
+    JPanel mainPanel;
+    JLabel mainTitle = new JLabel();
+    JButton buttonStart = new JButton();
+    JButton buttonExit = new JButton();
+    CardLayout crd = new CardLayout();
+    Container cPane;
+    public App() throws IOException { //settings of window
         super("BLACKJACK!!!");
         this.getContentPane().setBackground(Color.black);
         this.setLocationRelativeTo(null);
         this.setIconImage(ImageIO.read(new File("pics/icon.png")));
         this.setExtendedState(JFrame.MAXIMIZED_BOTH);
         this.setUndecorated(true);
-        this.setVisible(true);
-        this.setLayout(null);
+        this.setLayout(crd);
         this.validate();
 
-        main.setOpaque(true);
-        main.setVisible(true);
-        main.setIcon(new ImageIcon(ImageIO.read(new File("pics/bg.jpg"))));
-        main.setBounds(0,0,1920,1080);
-        this.setContentPane(main);
+        //title screen
+        title();
 
-        menu();
+        cPane = getContentPane();
+        crd = new CardLayout();
 
+        //rest of setting for window (these must be in the end)
+        this.setVisible(true);
+        this.validate();
+        this.pack();
     }
 
-    public void menu(){
-        buttonSetup(buttonStart,"Start","pics/start.png",500,300,180,50);
-        main.add(buttonStart);
-        buttonSetup(buttonExit,"Exit","pics/exit.png",1650,55,180,50);
-        main.add(buttonExit);
+    public void title() throws IOException{
+        //buttons
+        mainTitle.add(buttonSetup(buttonStart,"Start","pics/start.png",500,300,180,50));
+        mainTitle.add(buttonSetup(buttonExit,"Exit","pics/exit.png",1650,55,180,50));
 
+        mainTitle.setOpaque(true);
+        mainTitle.setVisible(true);
+        mainTitle.setIcon(new ImageIcon(ImageIO.read(new File("pics/bg.jpg"))));
+        mainTitle.setBounds(0,0,1920,1080);
+        this.setContentPane(mainTitle);
+
+        //text
+
+        mainTitle.setText("Welcome to BLACKJACK");
+        mainTitle.setHorizontalTextPosition(JLabel.CENTER);
+        mainTitle.setVerticalTextPosition(JLabel.TOP);
     }
 
     public void actionPerformed(ActionEvent e) {
@@ -53,7 +65,7 @@ public class App extends JFrame implements ActionListener{
         }
     }
 
-    public void buttonSetup(JButton but, String name, String filename, int x, int y, int width, int height){
+    public JButton buttonSetup(JButton but, String name, String filename, int x, int y, int width, int height){
         but.setVisible(true);
         but.validate();
         but.setText(name);
@@ -61,9 +73,10 @@ public class App extends JFrame implements ActionListener{
         but.setFocusable(false);
         but.setIcon(new ImageIcon(filename));
         but.setBounds(x,y,width,height);
-        but.setBackground(Color.lightGray);
+        but.setBackground(Color.white);
         but.setForeground(Color.black);
         but.setFont(new Font("Arial", Font.BOLD, 30));
-        but.setBorder(BorderFactory.createLineBorder(Color.green,3));
+        but.setBorder(BorderFactory.createLineBorder(Color.black,3));
+        return but;
     }
 }
